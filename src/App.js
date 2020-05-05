4d6b101d2da22cbfc76868373b2d8b54a18c7d76
jhaameehu <jamiuazeez49@gmail.com>
2020-05-05 07:44:40 -0700

    added styles bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -48,14 +48,30 @@ class App extends React.Component {
     return ( <
       div >
       <
+      div className = "wrapper" >
+      <
+      div className = "main" >
+      <
+      div className = "container" >
+      <
+      div className = "row" >
+      <
+      div className = "col-xs-5 title-container" >
+      <
       Titles / >
+      <
+      /div> <
+      div className = "col-xs-7 form-container" >
       <
       Form getWeather = {
         this.getWeather
       }
-      / > <
+      /> <
       Weather temperature = {
         this.state.temperature
+      }
+      humidity = {
+        this.state.humidity
       }
       city = {
         this.state.city
@@ -63,17 +79,19 @@ class App extends React.Component {
       country = {
         this.state.country
       }
-      humidity = {
-        this.state.humidity
-      }
       description = {
         this.state.description
       }
       error = {
         this.state.error
       }
-      / > < /
-      div >
+      /> < /
+      div > <
+      /div> < /
+      div > <
+      /div> < /
+      div > <
+      /div>
     );
   }
 };
